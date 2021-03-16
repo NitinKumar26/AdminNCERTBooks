9bb67c9dbfeb539c602ee49c3f39c93033703abd
Nitin Kumar <32798252+NitinKumar26@users.noreply.github.com>
2021-03-16 16:54:36 +0530

    changes

--- app/src/main/java/com/gadgetsfolk/admin/ncertbooks/UpdateDocs.java
+++ app/src/main/java/com/gadgetsfolk/admin/ncertbooks/UpdateDocs.java
@@ -2,10 +2,15 @@ package com.gadgetsfolk.admin.ncertbooks;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -17,10 +22,15 @@ public class UpdateDocs extends AppCompatActivity {
     private String className;
     private String docId;
     private String subjectName;
-    int startIndex = 169;
-    int endIndex = 193;
+    int startIndex = 165;
+
     String pdfName;
     String url;
+    private EditText edTvIndexValue;
+    private SwitchCompat switchIsNegative;
+    private Button btnUpdatePdfNames;
+    private boolean isNegative = false;
+    int indexValue;
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -33,37 +43,52 @@ public class UpdateDocs extends AppCompatActivity {
         docId = getIntent().getStringExtra(getString(R.string.doc_id));
         subjectName = getIntent().getStringExtra(getString(R.string.subject_name));
 
-        updatePdfNames(lang, type, className);
+        edTvIndexValue = findViewById(R.id.index_value);
+        switchIsNegative = findViewById(R.id.isNegative);
+        btnUpdatePdfNames = findViewById(R.id.btn_update_pdf_name);
+
+        edTvIndexValue.setText("0");
+
+        switchIsNegative.setOnCheckedChangeListener((buttonView, isChecked) -> isNegative = !isNegative);
+
+        btnUpdatePdfNames.setOnClickListener(v -> {
+            indexValue = Integer.parseInt(edTvIndexValue.getText().toString().trim());
+            if (indexValue > 0 && isNegative) indexValue = -indexValue;
+            Log.e("indexValue", String.valueOf(indexValue));
+            Log.e("isNegative", String.valueOf(isNegative));
+
+            updatePdfNames(lang, type, className, indexValue);
+        });
+
     }
 
-
-    private void updatePdfNames(String lang, String type, String className){
+    private void updatePdfNames(String lang, String type, String className, int index){
+        startIndex = startIndex + index;
         FirebaseFirestore.getInstance().collection(lang).document(type)
                 .collection("classes")
                 .document(className)
                 .collection("subjects")
                 .document(docId)
                 .collection("chapters").get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        for (int i = 0; i < queryDocumentSnapshots.getDocuments().size(); i++){
-                            url = queryDocumentSnapshots.getDocuments().get(i).getString("chapter_pdf_url");
-                            if (url != null) {
-                                Log.e("urlLength", String.valueOf(url.length()));
-                                pdfName = url.substring(startIndex, endIndex);
-                                FirebaseFirestore.getInstance().collection(lang).document(type)
-                                        .collection("classes")
-                                        .document(className)
-                                        .collection("subjects")
-                                        .document(docId)
-                                        .collection("chapters").document(queryDocumentSnapshots.getDocuments().get(i).getId())
-                                        .update("pdf_name", pdfName).addOnSuccessListener(unused ->
-                                        Toast.makeText(UpdateDocs.this, "Oh Yeah!", Toast.LENGTH_SHORT).show());
-                            }
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (int i = 0; i < queryDocumentSnapshots.getDocuments().size(); i++){
+                        url = queryDocumentSnapshots.getDocuments().get(i).getString("chapter_pdf_url");
+                        if (url != null) {
+                            Log.e("urlLength", String.valueOf(url.length()));
+                            pdfName = url.substring(startIndex, startIndex + 24);
+                            FirebaseFirestore.getInstance()
+                                    .collection(lang)
+                                    .document(type)
+                                    .collection("classes")
+                                    .document(className)
+                                    .collection("subjects")
+                                    .document(docId)
+                                    .collection("chapters").document(queryDocumentSnapshots.getDocuments().get(i).getId())
+                                    .update("pdf_name", pdfName).addOnSuccessListener(unused ->
+                                    Toast.makeText(UpdateDocs.this, "Oh Yeah!", Toast.LENGTH_SHORT).show());
                         }
                     }
-                }).addOnFailureListener(e -> {})
+                }).addOnFailureListener(e -> Log.e("exception", e.getMessage()))
                 .addOnCompleteListener(task -> {});
     }
 }
